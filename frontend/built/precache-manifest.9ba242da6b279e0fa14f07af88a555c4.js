97b7c287d4703de705707a7ae8a1c9372841c758
Jarv <kira-8@hotmail.com>
2019-02-24 18:56:02 +0100

    Last chance?

---
+++ frontend/built/precache-manifest.9ba242da6b279e0fa14f07af88a555c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb16d7d0c1dcf30313fc",
+    "url": "/static/js/main.bb16d7d0.chunk.js"
+  },
+  {
+    "revision": "896e1aad1a9a6274a154",
+    "url": "/static/js/1.896e1aad.chunk.js"
+  },
+  {
+    "revision": "bb16d7d0c1dcf30313fc",
+    "url": "/static/css/main.7bd8b9b9.chunk.css"
+  },
+  {
+    "revision": "9e328c02dd925854ffa6d2b3edaab033",
+    "url": "/index.html"
+  }
+];
