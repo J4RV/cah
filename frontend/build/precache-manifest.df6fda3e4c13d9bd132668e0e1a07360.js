097764d75440795eb977ddd74854686ec4ff28a8
Jarv <kira-8@hotmail.com>
2020-05-23 16:14:00 +0200

    Rebuilt

---
+++ frontend/build/precache-manifest.df6fda3e4c13d9bd132668e0e1a07360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de89473a8971985a36fefdc6dfe1e20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7af8b36e7397e793c798",
+    "url": "/static/css/main.b4d45b48.chunk.css"
+  },
+  {
+    "revision": "6153c53116942a12357a",
+    "url": "/static/js/2.f9097ec2.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.f9097ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af8b36e7397e793c798",
+    "url": "/static/js/main.38eb68b2.chunk.js"
+  },
+  {
+    "revision": "35e30a9aed35b99275cb",
+    "url": "/static/js/runtime-main.964a35a7.js"
+  }
+]);
